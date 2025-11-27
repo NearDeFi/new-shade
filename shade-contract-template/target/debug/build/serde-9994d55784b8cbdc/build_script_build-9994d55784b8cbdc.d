3cfc853404c0968e3e6e4c4d3357c09715d1d3ad
PiVortex <owengeorgehassall@gmail.com>
2025-11-27 12:34:23 -0300

    add git ignore

--- shade-contract-template/target/debug/build/serde-9994d55784b8cbdc/build_script_build-9994d55784b8cbdc.d
+++
@@ -1,5 +0,0 @@
-/Users/owen/projects/proximity/shade-contract-template/target/debug/build/serde-9994d55784b8cbdc/build_script_build-9994d55784b8cbdc: /Users/owen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/Users/owen/projects/proximity/shade-contract-template/target/debug/build/serde-9994d55784b8cbdc/build_script_build-9994d55784b8cbdc.d: /Users/owen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/Users/owen/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:
